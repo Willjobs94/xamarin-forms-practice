305b1703c74e8e8e881139df56faddfaa1bd17d3
William Bello <willjobs94@gmail.com>
2016-09-16 12:42:18 -0400

    Add CodePlusXaml solution

---
+++ CodePlusXaml/CodePlusXaml/CodePlusXamlPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CodePlusXaml
+{
+	public partial class CodePlusXamlPage : ContentPage
+	{
+		public CodePlusXamlPage()
+		{
+			InitializeComponent();
+
+			var label = new Label
+			{
+				Text = "Hello From code",
+				TextColor = Color.Blue,
+				FontSize = Device.GetNamedSize(NamedSize.Large, this),
+				BackgroundColor = Color.FromHex("#FF8080"),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.Center,
+				HorizontalTextAlignment = TextAlignment.Center,
+				Opacity = 0.75,
+				FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+			};
+
+			(Content as StackLayout).Children.Insert(0,label);
+		}
+	}
+}
