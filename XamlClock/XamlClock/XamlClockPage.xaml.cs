cf40e668ef40a0dc42b5b5e08cd28fbdf814729e
William Bello <willjobs94@gmail.com>
2016-09-26 16:22:28 -0400

    Add XamlClock solution

---
+++ XamlClock/XamlClock/XamlClockPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using System;
+namespace XamlClock
+{
+	public partial class XamlClockPage : ContentPage
+	{
+		public XamlClockPage()
+		{
+			InitializeComponent();
+
+			Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+		}
+
+		bool OnTimerTick()
+		{
+			var dt = DateTime.Now;
+			timeLabel.Text = dt.ToString("T");
+			dateLabel.Text = dt.ToString("D");
+			return true;
+		}
+}
+}
