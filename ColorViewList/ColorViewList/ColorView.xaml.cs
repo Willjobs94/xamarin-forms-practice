72e31db8c6445ed84f68dd6f1f58504e6d0ec1df
William Bello <willjobs94@gmail.com>
2016-09-29 23:56:31 -0400

    Add MonkeyAppTap solution

--- ColorViewList/ColorViewList/ColorView.xaml.cs
+++ ColorViewList/ColorViewList/ColorView.xaml.cs
@@ -6,9 +6,30 @@ namespace ColorViewList
 {
 	public partial class ColorView : ContentView
 	{
+		string colorName;
+		ColorTypeConverter colorTypeConverter = new ColorTypeConverter();
 		public ColorView()
 		{
 			InitializeComponent();
+		}
+
+		public string ColorName { 
+			get 
+			{
+				return colorName;	
+			} 
+			set
+			{
+				colorName = value;
+				colorNameLabel.Text = value;
+
+				var color = (Color)colorTypeConverter.ConvertFromInvariantString(colorName);
+				boxView.Color = color;
+				colorValueLabel.Text = string.Format("{0:X2}-{1:X2}-{2:X2}",
+													 (int)(255 * color.R),
+													 (int)(255 * color.G),
+													 (int)(255 * color.B));
+			}
 		}
 	}
 }
---
+++ MonkeyTap/MonkeyTap/MonkeyTapPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MonkeyTap
+{
+	public partial class MonkeyTapPage : ContentPage
+	{
+		const int sequenceTime = 750;
+		protected int flashDuration = 250;
+
+		const double offLuminosity = 0.4;
+		const double onLuminosity = 0.75;
+		BoxView[] boxViews;
+		Color[] colors = { Color.Red, Color.Blue, Color.Yellow, Color.Green };
+		List<int> sequence = new List<int>();
+		int sequenceIndex;
+		bool awatingTaps;
+		bool gameEnded;
+		Random random = new Random();
+
+		public MonkeyTapPage()
+		{
+			InitializeComponent();
+			boxViews = new BoxView[] { boxView0, boxView1, boxView2, boxView3 };
+			InitializeBoxViewColors();
+		}
+
+		void InitializeBoxViewColors()
+		{
+			for (var i = 0; i < 4; i++)
+			{
+				boxViews[i].Color = colors[i].WithLuminosity(offLuminosity);
+			}
+		}
+
+		public void OnBoxViewTapped(object sender, EventArgs args)
+		{
+			if (gameEnded) return;
+			if (!awatingTaps) 
+			{
+				EndGame();
+				return;
+			}
+
+			var tappedBoxView = sender as BoxView;
+			var index = Array.IndexOf(boxViews, tappedBoxView);
+
+			if (index != sequence[sequenceIndex])
+			{
+				EndGame();
+				return;
+			}
+
+			FlashBoxView(index);
+			sequenceIndex++;
+			awatingTaps = sequenceIndex < sequence.Count;
+			if (!awatingTaps) StartSequence();
+			    
+		}
+
+		void EndGame()
+		{
+			gameEnded = true;
+			for (int i = 0; i < 4; i++)
+			{
+				boxViews[i].Color = Color.Gray;
+			}
+
+			startGameButton.Text = "Try Again";
+			startGameButton.IsVisible = true;
+
+		}
+
+		public void OnStartButtonClicked(object sender, EventArgs args)
+		{
+			gameEnded = false;
+			startGameButton.IsVisible = false;
+			InitializeBoxViewColors();
+			sequence.Clear();
+			StartSequence();
+		}
+
+		void StartSequence()
+		{
+			sequence.Add(random.Next(4));
+			sequenceIndex = 0;
+			Device.StartTimer(TimeSpan.FromMilliseconds(1), OnTimerTick);
+		}
+
+		bool OnTimerTick()
+		{
+			if (gameEnded) return false;
+
+			FlashBoxView(sequence[sequenceIndex]);
+			sequenceIndex++;
+			awatingTaps = sequenceIndex == sequence.Count;
+			sequenceIndex = awatingTaps ? 0 : sequenceIndex;
+			return !awatingTaps;
+		}
+
+		void FlashBoxView(int index)
+		{
+			boxViews[index].Color = colors[index].WithLuminosity(onLuminosity);
+			Device.StartTimer(TimeSpan.FromMilliseconds(1), () =>
+			{
+				if (gameEnded) return false;
+
+				boxViews[index].Color = colors[index].WithLuminosity(offLuminosity);
+				return false;
+
+			});
+
+		}
+	}
+}
